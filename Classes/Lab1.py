0efbd73378f9bf67c175e519b9898b8dff527a35
renanfavero30 <renanfavero@ufl.edu>
2023-01-19 17:43:57 -0500

    Lab 1

---
+++ Classes/Lab1.py
@@ -0,0 +1,12 @@
+#Lab 1 - Renan Favero
+
+#including the name
+name = input("Hello. What is your name?")
+
+#including age
+age = int(input(f"It's nice to meet you, {name}. How old are you?"))
+print(f"I see that you are still quite young at only {age}.")
+
+#including city
+city = input("Where do you live?")
+print(f"Wow! I've always wanted to go to {city}. Thanks for chatting with me. Bye!")
---
+++ Classes/Week_test.py
@@ -0,0 +1,9 @@
+#this is a test
+"""
+Clone - daolownd
+add - track the changes of a folder
+commig save yout file in GIT  :     git commit -m  "commit test"
+Push uploand to GIT           : git push -f origin master
+
+Pull dowloand changes
+"""
